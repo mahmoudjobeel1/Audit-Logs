7f8b4c1e37bfbaa701aa7fa6ef4b5893fbab52a4
mahmoudjobeel <mahmoudjobeel@gmail.com>
2024-03-30 11:36:41 +0200

    Add Table Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import SearchInput from "./Components/SearchInput";
+import Table from "./Components/Table";
 
 function App() {
   return (
     <section className="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
-      <div className="mx-auto max-w-screen-xl px-4 lg:px-12">
+      <div className="mx-auto max-w-screen-xl px-4 lg:px-12 space-y-3 ">
         <SearchInput />
         {/* Filters */}
-        {/* Table */}
+        <Table />
       </div>
     </section>
   );
