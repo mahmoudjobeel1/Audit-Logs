5a795fed419614485934e2d59c0f1a461f84d545
mahmoudjobeel <mahmoudjobeel@gmail.com>
2024-04-02 12:59:56 +0200

    Refactor Logs Table

--- frontend/src/Components/Table/index.tsx
+++ frontend/src/Components/Table/index.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
 import { LeftArraw } from "../../assests/svgIcons";
+import { Table } from "flowbite-react";
 
-export function RowTable() {
+export function LogRowTable() {
+  const [showInfo, setShowInfo] = useState(false);
+
   const shortFormatedData = (dateText: string) => {
     const date = moment(dateText);
     const formattedDate = date.format("MMM D, h:mm A");
@@ -12,55 +15,34 @@ export function RowTable() {
 
   return (
     <>
-      <tr className="border-b dark:border-gray-700">
-        <td className="px-4 py-3 flex items-center justify-cente space-x-2">
-          <span className="inline-flex items-center justify-center size-[46px] text-sm font-semibold leading-none rounded-full border border-gray-500 text-gray-500 dark:text-gray-400">
-            AC
-          </span>
-          <div>Mahmoud</div>
-        </td>
-        <td className="px-4 py-3">user.login_succeeded</td>
-        <td className="px-4 py-3">
-          {shortFormatedData("2022-01-05T14:31:13.607Z")}
-        </td>
-        <td className="px-4 py-3 flex items-center justify-end">
-          <button
-            id="apple-imac-20-dropdown-button"
-            data-dropdown-toggle="apple-imac-20-dropdown"
-            className="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
-            type="button"
-          >
+      <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+        <Table.Cell>Mahmoud</Table.Cell>
+        <Table.Cell>user.login_succeeded</Table.Cell>
+        <Table.Cell>{shortFormatedData("2022-01-05T14:31:13.607Z")}</Table.Cell>
+        <Table.Cell>
+          <button type="button" onClick={() => setShowInfo(!showInfo)}>
             {LeftArraw}
           </button>
-        </td>
-      </tr>
+        </Table.Cell>
+      </Table.Row>
     </>
   );
 }
-export default function Table() {
+export default function LogsTable() {
   return (
-    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-        <tr>
-          <th scope="col" className="px-4 py-3">
-            Actor
-          </th>
-          <th scope="col" className="px-4 py-3">
-            Action
-          </th>
-          <th scope="col" className="px-4 py-3">
-            Date
-          </th>
-          <th scope="col" className="px-4 py-3" />
-        </tr>
-      </thead>
-      <tbody>
-        <RowTable />
-        <RowTable />
-        <RowTable />
-        <RowTable />
-        <RowTable />
-      </tbody>
-    </table>
+    <div className="overflow-x-auto">
+      <Table hoverable>
+        <Table.Head>
+          <Table.HeadCell>Actor</Table.HeadCell>
+          <Table.HeadCell>Action</Table.HeadCell>
+          <Table.HeadCell>Date</Table.HeadCell>
+          <Table.HeadCell />
+        </Table.Head>
+        <Table.Body className="divide-y">
+          <LogRowTable />
+          <LogRowTable />
+        </Table.Body>
+      </Table>
+    </div>
   );
 }
--- frontend/src/assests/svgIcons.tsx
+++ frontend/src/assests/svgIcons.tsx
@@ -6,17 +6,17 @@ export const LeftArraw = (
     viewBox="0 0 20 20"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g id="SVGRepo_bgCarrier" stroke-width="0" />
+    <g id="SVGRepo_bgCarrier" strokeWidth="0" />
     <g
       id="SVGRepo_tracerCarrier"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <g id="SVGRepo_iconCarrier">
       <path
         d="M10 7L15 12L10 17"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </g>
   </svg>
